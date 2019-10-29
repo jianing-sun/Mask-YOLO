402dbd945846ae2163f28abe92369a00c947a549
Jianing Sun <jianing.sun@mail.mcgill.ca>
2019-10-30 06:51:06 +0800

    Update myolo_utils.py

--- myolo/myolo_utils.py
+++ myolo/myolo_utils.py
@@ -924,3 +924,40 @@ def resize_one_image(image, gt_box, gt_mask, new_shape):
 
     gt_box[0], gt_box[2] = max(min(gt_box[0], new_w), 0), max(min(gt_box[2], new_w), 0)
     gt_box[1], gt_box[3] = max(min(gt_box[1], new_h), 0), max(min(gt_box[3], new_h), 0)
+    
+# from mrcnn keras\&tensorflow implementation    
+def batch_slice(inputs, graph_fn, batch_size, names=None):
+    """Splits inputs into slices and feeds each slice to a copy of the given
+    computation graph and then combines the results. It allows you to run a
+    graph on a batch of inputs even if the graph is written to support one
+    instance only.
+
+    inputs: list of tensors. All must have the same first dimension length
+    graph_fn: A function that returns a TF tensor that's part of a graph.
+    batch_size: number of slices to divide the data into.
+    names: If provided, assigns names to the resulting tensors.
+    """
+    if not isinstance(inputs, list):
+        inputs = [inputs]
+
+    outputs = []
+    for i in range(batch_size):
+        inputs_slice = [x[i] for x in inputs]
+        output_slice = graph_fn(*inputs_slice)
+        if not isinstance(output_slice, (tuple, list)):
+            output_slice = [output_slice]
+        outputs.append(output_slice)
+    # Change outputs from a list of slices where each is
+    # a list of outputs to a list of outputs and each has
+    # a list of slices
+    outputs = list(zip(*outputs))
+
+    if names is None:
+        names = [None] * len(outputs)
+
+    result = [tf.stack(o, axis=0, name=n)
+              for o, n in zip(outputs, names)]
+    if len(result) == 1:
+        result = result[0]
+
+    return result
